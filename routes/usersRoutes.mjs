23c87d8afcac182b6cda84afbaaac4bfcc416cc2
Enricrypto <105727501+Enricrypto@users.noreply.github.com>
2024-05-17 14:33:59 +0200

    fix: userRoutes

--- routes/usersRoutes.mjs
+++ routes/usersRoutes.mjs
@@ -7,7 +7,7 @@ import {
 
 const router = express.Router()
 
-router.post('/create-profile', updateUserProfile)
+router.post('/update-user/:id', updateUserProfile)
 router.get('/', getAllUsers)
 
 export default router
