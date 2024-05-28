6b55b7921680ebce9e081a8d6045385acdca0373
Nuno Rodrigues <123973723+NVR-2023@users.noreply.github.com>
2024-05-28 20:22:03 +0100

    chore:

--- routes/groupsRoutes.mjs
+++ routes/groupsRoutes.mjs
@@ -1,11 +1,8 @@
-// Contains the route files that define the endpoints for each part of your application.
-//These files typically import the controllers to handle requests.
+
 import express from "express";
 import { createGroup } from "../controllers/groupsControllers.mjs";
 
 const router = express.Router();
 
-// here you have all the routes
 router.post("/create-group", createGroup);
-
 export default router;
